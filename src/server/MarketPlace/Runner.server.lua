ebf2afe33084165863716045a0aee45c43b4915f
Archie <protocoladvancedwas@gmail.com>
2022-06-25 14:16:30 +0100

    main

--- src/server/MarketPlace/Runner.server.lua
+++ src/server/MarketPlace/Runner.server.lua
@@ -2,8 +2,11 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local RunnerEvents = ReplicatedStorage:WaitForChild("MarketPlace")
 
+local Players = game:GetService("Players")
 local CollectionService = game:GetService("CollectionService")
 
 game:GetService('Players').PlayerAdded:Connect(function(player)
-    
+    if player:GetRankInGroup(3043431) >= 3 then
+        local Team = game.Teams:Finds
+    end
 end)
